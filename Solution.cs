98e2927ab8c92d1ce29700273d12bcc12f0912be
unknown <pulok.rahman.630@my.csun.edu>
2023-03-28 08:48:29 -0700

    Added Solution

---
+++ Solution.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.IsPalindrome(10));
+        Console.WriteLine(solution.IsPalindrome(3003));
+
+    }
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+        String y = x.ToString();
+        int a = 0;
+        int b = y.Length - 1;
+        while (a < b)
+        {
+            if (y[b] != y[a])
+                return false;
+            a++;
+            b--;
+
+
+        }
+        return true;
+
+
+
+
+    }
+}
