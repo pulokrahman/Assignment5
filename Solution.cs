b35f603bd357c3a69abf794e98d118292b9e78ea
unknown <pulok.rahman.630@my.csun.edu>
2023-03-28 09:26:23 -0700

    Added Solution

--- Solution.cs
+++ Solution.cs
@@ -7,6 +7,9 @@
         Console.WriteLine(solution.IsPalindrome(3003));
         Console.WriteLine(solution.ContainsDuplicate( new int[]{ 1,2,3}));
         Console.WriteLine(solution.ContainsDuplicate(new int[] { 1, 2, 1 }));
+        String[] fizz = (String[]) solution.FizzBuzz(15);
+        for (int i = 0; i < fizz.Length; i++)
+            Console.WriteLine(fizz[i]);
     }
     public bool IsPalindrome(int x)
     {
@@ -29,6 +32,23 @@
 
 
 
+    }
+    public IList<string> FizzBuzz(int n)
+    {
+        String[] ans = new String[n];
+        for (int i = 0; i < n; i++)
+        {
+            if ((i + 1) % 15 == 0)
+                ans[i] = "FizzBuzz";
+            else if ((i + 1) % 5 == 0)
+                ans[i] = "Buzz";
+            else if ((i + 1) % 3 == 0)
+                ans[i] = "Fizz";
+            else
+                ans[i] = Convert.ToString(i + 1);
+
+        }
+        return ans;
     }
     public bool ContainsDuplicate(int[] nums)
     {
